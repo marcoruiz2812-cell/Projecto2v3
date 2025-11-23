0f7dcaaa75780d8a241a41ad57da0aa8113ddab3
marcoruiz2812-cell <marcoruiz2812@gmail.com>
2025-11-23 00:01:37 -0500

    Clasificación y pegado de las imagenes concluido

--- aprendiendo_digits.py
+++ aprendiendo_digits.py
@@ -1,18 +1,76 @@
-import math
 from sklearn import datasets
+import numpy as np
 
-misdatos = datasets.load_digits()
 
-X= misdatos.data
-y= misdatos.target
+datos = datasets.load_digits()
+
+
+#distintos digitos procesados para ser comparados de varias formas
+X= datos.data
+y= datos.target
+
+data = np.loadtxt("numeros.csv", delimiter=",", skiprows=1)
+
+digitos = data[:, :64]
+
+#Flatten
+vectores = [mat.flatten() for mat in digitos]
 
 def distancia_eucli(a,b):
     suma= 0
     for i in range(len(a)):
         suma += (a[i]-b[i])**2
-    return math.sqrt(suma)
+    return suma**0.5
 
-distancias=[]
-for i in range(len(X)):
-    d= distancia_eucli(nose, X[i])
-    distancias.append(d)
+indicevector=0
+#Distancias acumuladas por digito
+distancia_Todos=[]
+for digito in range(len(vectores)):
+    distancia1=[]
+    #distancias por digito
+    for i in range(len(X)):
+        d=distancia_eucli(vectores[digito],X[i])
+        distancia1.append(d)
+    distancia_Todos.append(distancia1)
+
+
+reales = [i for i in range(10) for _ in range(6)]
+
+for dig_index in range(len(distancia_Todos)):
+    #el digito en cuestion y sus distancias
+    dig=distancia_Todos[dig_index]
+    #indices de la lista
+    indices = list(range(len(dig)))
+
+    for i in range(len(indices)):
+        for j in range(i+1, len(dig)):
+            if dig[j]<dig[i]:
+                temp=indices[i]
+                indices[i]=indices[j]
+                indices[j]=temp
+    #en las listas ordenadas
+    vecinos= indices[:3]
+
+    #numeros reales
+    targets = [int(y[i]) for i in vecinos]
+    d={}
+    for i in range(len(targets)):
+        if targets[i] not in d:
+            d[targets[i]] = 1
+        else:
+            d[targets[i]] += 1
+
+    vecs=[]
+    repes=[]
+    for v,r in d.items():
+        vecs.append(v)
+        repes.append(r)
+
+    for i in range(len(repes)):
+        if repes[i]==max(repes):
+            resultado=vecs[i]
+    print(f"Numero real: {reales[dig_index]}")
+    print(f"Sus vecinos: {targets}")
+    print(f"Soy la inteligencia artificial, y he detectado que el dígito ingresado corresponde al número {resultado}")
+
+
--- lectura_de_imagen_incompleto.py
+++
@@ -1,9 +0,0 @@
-# instalar opencv-python
-
-import cv2
-
-img_array = cv2.imread("Datasets/miNumero.png", cv2.IMREAD_GRAYSCALE)
-#print(img_array)
-nueva_img = cv2.resize(img_array, (8,8))
-
-print(nueva_img)
